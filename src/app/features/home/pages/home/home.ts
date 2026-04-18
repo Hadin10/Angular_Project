caff7e0768bc12886a0341d5c9c6a6b3696d9607
Md Nura Akram <mdnura.akram@hpl.com.bd>
2026-04-18 11:59:40 +0600

    Some Changes

--- src/app/features/home/pages/home/home.ts
+++ src/app/features/home/pages/home/home.ts
@@ -14,15 +14,15 @@ export class Home {
 
   products = [
     { id: 1,  name: 'সুন্দরবনের মধু',  price: 1250, discount: 1400, unit: 'kg', rating: Array.from({ length: 5 }), image: 'images/picture1.png' },
-    { id: 2,  name: 'Product 2',  price: 50, discount: 60, unit: 'kg', rating: Array.from({ length: 5 }), image: 'images/picture2.png' },
-    { id: 3,  name: 'Product 3',  price: 50, discount: 60, unit: 'kg', rating: Array.from({ length: 5 }), image: 'images/picture3.png' },
-    { id: 4,  name: 'Product 4',  price: 50, discount: 60, unit: 'kg', rating: Array.from({ length: 5 }), image: 'images/picture4.png' },
-    { id: 5,  name: 'Product 5',  price: 50, discount: 60, unit: 'kg', rating: Array.from({ length: 5 }), image: 'images/picture5.png' },
-    { id: 6,  name: 'Product 6',  price: 50, discount: 60, unit: 'kg', rating: Array.from({ length: 5 }), image: 'images/picture6.png' },
-    { id: 7,  name: 'Product 7',  price: 50, discount: 60, unit: 'kg', rating: Array.from({ length: 5 }), image: 'images/picture7.png' },
-    { id: 8,  name: 'Product 8',  price: 50, discount: 60, unit: 'kg', rating: Array.from({ length: 5 }), image: 'images/picture8.png' },
-    { id: 9,  name: 'Product 9',  price: 50, discount: 60, unit: 'kg', rating: Array.from({ length: 5 }), image: 'images/picture9.png' },
-    { id: 10, name: 'Product 10', price: 50, discount: 60, unit: 'kg', rating: Array.from({ length: 5 }), image: 'images/picture10.png' },
+    { id: 2,  name: 'সরিষা ফুলের মধু',  price: 1000, discount: 800, unit: 'kg', rating: Array.from({ length: 5 }), image: 'images/picture2.png' },
+    { id: 3,  name: 'হানি নাটস',  price: 50, discount: 60, unit: 'kg', rating: Array.from({ length: 5 }), image: 'images/picture3.png' },
+    { id: 4,  name: 'কাজু বাদাম',  price: 50, discount: 60, unit: 'kg', rating: Array.from({ length: 5 }), image: 'images/picture4.png' },
+    { id: 5,  name: 'কাঠ বাদাম',  price: 50, discount: 60, unit: 'kg', rating: Array.from({ length: 5 }), image: 'images/picture5.png' },
+    { id: 6,  name: 'পেস্তা বাদাম',  price: 50, discount: 60, unit: 'kg', rating: Array.from({ length: 5 }), image: 'images/picture6.png' },
+    { id: 7,  name: 'চিনা বাদাম',  price: 50, discount: 60, unit: 'kg', rating: Array.from({ length: 5 }), image: 'images/picture7.png' },
+    { id: 8,  name: 'আখরোট',  price: 50, discount: 60, unit: 'kg', rating: Array.from({ length: 5 }), image: 'images/picture8.png' },
+    { id: 9,  name: 'সরিষার তেল',  price: 50, discount: 60, unit: 'kg', rating: Array.from({ length: 5 }), image: 'images/picture9.png' },
+    { id: 10, name: 'চিয়া সিড', price: 50, discount: 60, unit: 'kg', rating: Array.from({ length: 5 }), image: 'images/picture10.png' },
   ];
 
   addToCart(product: any) {
